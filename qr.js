7a12e9cbe5e53b0ad1dabe0f035284123d491589
Popkid <popkidtechs@gmail.com>
2025-01-05 00:23:26 +0300

    ⭕POP KID⭕

--- qr.js
+++ qr.js
@@ -1,5 +1,5 @@
 // get qr  code of any bot using this ....................
-//coded  by wasi
+//coded  by popkid
 
 
 const express = require("express");
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *POPKID-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/Popkiddevs_
+❒ *Author:* _wa.me/254111385747_
+❒ *YT:* _https://youtube.com/@Pop_kid254_
 └────────────⭓
 `
 
@@ -134,7 +134,7 @@ SESSION-ID ==> ${Scan_Id}
     await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
 
 
-    //// MADE BY ITXWASI
+    //// MADE BY popkid
 
 });
 
